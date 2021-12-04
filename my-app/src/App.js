85246a4088bdcfba880d4e2a0d4c5924100ed849
nouria chaouy <nouria.chaouy@gmail.com>
2021-12-04 14:59:18 +0000

    modified routes and signup / signin pages

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,27 @@
 import React from "react";
 import './App.css';
 import Navbar from "./components/Navbar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import SignUp from './pages/Signup';
-import SignIn from './pages/Signin';
+import Signup from './pages/Signup';
+import Signin from './pages/Signin';
 import Profile from "./pages/Profile";
 import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
-    <Router>
+    <BrowserRouter>
       <Navbar />
-      <Home path="/" />
-      <About path="/about" />
-      <Profile path="/profile" />
-      <SignIn path="/signin" />
-      <SignUp path="/sign-up" />
-    </Router>
+      <Routes>
+         <Route path="/" element={<Home />}/>
+         <Route path="/about" element={<About />}/>
+         <Route path="/profile" element={<Profile />}/>
+         <Route path="/signin" element={<Signin />}/>
+         <Route path="/signup" element={<Signup />}/>
+       </Routes>
+       </BrowserRouter>
     <Footer />
     </>
   );
--- my-app/src/pages/Signin.jsx
+++ my-app/src/pages/Signin.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import './Signin.css'
+import { useState } from 'react';
 
-const SignIn = () => {
-  return (
-    <main>
-        SignIn page
-    </main>
-  );
-};
+export default function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-export default SignIn;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(email, password)
+
+  }
+
+    return (
+        <form className="auth-form" onSubmit={handleSubmit}>
+        <h2>Log In</h2>
+        <label>
+            <span>email:</span>
+            <input
+            required
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            />
+        </label>
+        <label>
+            <span>password:</span>
+            <input
+            required
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
+            />
+        </label>
+        <button>login</button>
+        {/* {!isPending && <button className="btn">Login</button>}
+        {isPending && <button className="btn" disabled>Loading...</button>}
+        {error && <div className="error">{error}</div>} */}
+    </form>
+    )
+}
