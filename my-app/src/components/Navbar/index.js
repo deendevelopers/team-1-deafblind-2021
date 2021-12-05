3cc3edb5967f5a964d603123a63a33611f38cf84
nouria chaouy <nouria.chaouy@gmail.com>
2021-12-05 20:53:26 +0000

    last changes

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -11,16 +11,12 @@ import {
 import { useLogout } from "../../hooks/useLogout";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import AvatarPro from "../AvatarPro";
-<<<<<<< HEAD
 import { Box, Image } from "@chakra-ui/react";
-=======
->>>>>>> 8fbab7da8af3af62f6603429e47550ba3e1c3ebc
 
 const Navbar = () => {
   const { logout, isPending } = useLogout();
   const { user } = useAuthContext();
 
-<<<<<<< HEAD
   return (
     <>
       <Nav>
@@ -70,52 +66,5 @@ const Navbar = () => {
       </Nav>
     </>
   );
-=======
-    return (
-        <>
-           <Nav>
-            <NavLogo to="/">
-                App Name
-            </NavLogo>
-            <Bars />
-
-            <NavMenu>
-                <NavLink to="/" activeStyle>
-                    Home
-                </NavLink>
-                <NavLink to="/findvolunteers" activeStyle>
-                    Find Volunteers
-                </NavLink>
-                {/* <NavLink to="/about" activeStyle>
-                    About
-                </NavLink> */}
-                {user && (
-                <NavLink to="/profile" activeStyle>
-                    My Profile
-                </NavLink>
-                )}
-                {!user && (
-                    <>
-                <NavLink to="/signin" activeStyle>
-                    Login
-                </NavLink>
-                <NavLink to="/signup" activeStyle>
-                    Join Us
-                </NavLink>
-                </>
-                )}
-                {user && (
-                <>
-                <AvatarPro src={user.photoURL}/>
-                {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-                {isPending && <button className="btn" disabled>Logging out...</button>}
-                </>
-                )}
-                
-            </NavMenu> 
-           </Nav> 
-        </>
-    );
->>>>>>> 8fbab7da8af3af62f6603429e47550ba3e1c3ebc
 };
 export default Navbar;
