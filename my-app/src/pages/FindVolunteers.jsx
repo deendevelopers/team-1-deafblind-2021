65f34f5ff192966c98847834802ea2ec77fe5e6e
unknown <h.mohiadin.13@gmail.com>
2021-12-05 00:15:13 +0000

    add find volunteers page

--- my-app/src/pages/FindVolunteers.jsx
+++ my-app/src/pages/FindVolunteers.jsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Grid, Box } from "@chakra-ui/layout";
 import Suggestions from "../components/Dashboard/Suggestions";
+import TaskSuggestions from "../components/FindVolunteers/TaskSuggesions";
 
 const FindVolunteers = () => {
     return(
-        <Suggestions/>
+        <Grid templateColumns='repeat(2, 1fr)' gap={10}>
+            <Box>
+                <TaskSuggestions/>
+            </Box>
+            <Box>
+                <Suggestions/>
+            </Box>
+        </Grid>
     )
 };
 
