3194a1c6db44088603a6dfd14e8841777669b290
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-05 16:36:17 +0000

    added chakra to login page

--- my-app/src/pages/Signin.js
+++ my-app/src/pages/Signin.js
@@ -1,42 +1,67 @@
-import './Signin.css'
-import { useState } from 'react';
-import { useLogin } from '../hooks/useLogin';
+import "./Signin.css";
+import { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
+import {
+  Box,
+  Heading,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  VStack,
+} from "@chakra-ui/react";
 
 export default function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { login, error, isPending } = useLogin('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { login, error, isPending } = useLogin("");
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    login(email, password)
+    e.preventDefault();
+    login(email, password);
+  };
 
-  }
-
-    return (
-        <form className="auth-form" onSubmit={handleSubmit}>
-        <h2>Log In</h2>
-        <label>
-            <span>email:</span>
-            <input
-            required
-            type="email"
-            onChange={(e) => setEmail(e.target.value)}
-            value={email}
-            />
-        </label>
-        <label>
-            <span>password:</span>
-            <input
-            required
-            type="password"
-            onChange={(e) => setPassword(e.target.value)}
-            value={password}
-            />
-        </label>
-        {!isPending && <button className="btn">Login</button>}
-        {isPending && <button className="btn" disabled>Loading...</button>}
-        {error && <div className="error">{error}</div>}
-    </form>
-    )
+  return (
+    <Flex width="full" align="center" justifyContent="center">
+      <Box p={2}>
+        <Box textAlign="center">
+          <Heading size="lg">Log In</Heading>
+        </Box>
+        <Box my={4} textAlign="left">
+          <form className="auth-form" onSubmit={handleSubmit}>
+            <FormControl>
+              <FormLabel>Email</FormLabel>
+              <Input
+                required
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+            </FormControl>
+            <FormControl mt={6}>
+              <FormLabel>Password</FormLabel>
+              <Input
+                required
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+            </FormControl>
+            {!isPending && (
+              <Button width="full" mt={4} type="submit">
+                Login
+              </Button>
+            )}
+            {isPending && (
+              <button className="btn" disabled>
+                Loading...
+              </button>
+            )}
+            {error && <div className="error">{error}</div>}
+          </form>
+        </Box>
+      </Box>
+    </Flex>
+  );
 }
