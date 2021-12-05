b1470508dc86b0d817b4559d120585eecbf3e8f4
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-05 19:31:41 +0000

    replace app name with spectro

--- my-app/src/components/Navbar/index.js
+++ my-app/src/components/Navbar/index.js
@@ -1,68 +1,70 @@
 import React from "react";
 import {
-    Nav,
-    NavLogo,
-    NavLink,
-    Bars,
-    NavMenu,
-    NavBtn,
-    NavBtnLink,
+  Nav,
+  NavLogo,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink,
 } from "./NavbarElements";
-import { useLogout } from '../../hooks/useLogout'
-import { useAuthContext } from '../../hooks/useAuthContext';
+import { useLogout } from "../../hooks/useLogout";
+import { useAuthContext } from "../../hooks/useAuthContext";
 import AvatarPro from "../AvatarPro";
-
-
-
+import { Box, Image } from "@chakra-ui/react";
 
 const Navbar = () => {
-    const { logout, isPending } = useLogout()
-    const { user } = useAuthContext()
+  const { logout, isPending } = useLogout();
+  const { user } = useAuthContext();
 
-    return (
-        <>
-           <Nav>
-            <NavLogo to="/">
-                App Name
-            </NavLogo>
-            <Bars />
-
-            <NavMenu>
-                <NavLink to="/" activeStyle>
-                    Home
-                </NavLink>
-                <NavLink to="/findvolunteers" activeStyle>
-                    Find Volunteers
-                </NavLink>
-                <NavLink to="/about" activeStyle>
-                    About
-                </NavLink>
-                {user && (
-                <NavLink to="/profile" activeStyle>
-                    My Profile
-                </NavLink>
-                )}
-                {!user && (
-                    <>
-                <NavLink to="/signin" activeStyle>
-                    Login
-                </NavLink>
-                <NavLink to="/signup" activeStyle>
-                    Join Us
-                </NavLink>
-                </>
-                )}
-                {user && (
-                <>
-                <AvatarPro src={user.photoURL}/>
-                {!isPending && <button className="btn" onClick={logout}>Logout</button>}
-                {isPending && <button className="btn" disabled>Logging out...</button>}
-                </>
-                )}
-                
-            </NavMenu> 
-           </Nav> 
-        </>
-    );
+  return (
+    <>
+      <Nav>
+        <NavLogo to="/">Spectro</NavLogo>
+        <Bars />
+        <NavMenu>
+          <NavLink to="/" activeStyle>
+            Home
+          </NavLink>
+          <NavLink to="/findvolunteers" activeStyle>
+            Find Volunteers
+          </NavLink>
+          <NavLink to="/about" activeStyle>
+            About
+          </NavLink>
+          {user && (
+            <NavLink to="/profile" activeStyle>
+              My Profile
+            </NavLink>
+          )}
+          {!user && (
+            <>
+              <NavLink to="/signin" activeStyle>
+                Login
+              </NavLink>
+              <NavLink to="/signup" activeStyle>
+                Join Us
+              </NavLink>
+            </>
+          )}
+          {user && (
+            <>
+              <AvatarPro src={user.photoURL} />
+              {!isPending && (
+                <button className="btn" onClick={logout}>
+                  Logout
+                </button>
+              )}
+              {isPending && (
+                <button className="btn" disabled>
+                  Logging out...
+                </button>
+              )}
+            </>
+          )}
+        </NavMenu>
+      </Nav>
+    </>
+  );
 };
 export default Navbar;
