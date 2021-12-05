6dbf3b555447588ff33aab29951d25dd40614d69
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-12-05 17:18:33 +0000

    add chakra to sign up page

--- my-app/src/components/Dashboard/Welcome.jsx
+++ my-app/src/components/Dashboard/Welcome.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import { Container, VStack, Grid, Box, Heading, Button, Center, Avatar, Text, GridItem,  } from '@chakra-ui/react'
-import { useAuthContext } from '../hooks/useAuthContext';
+import {
+  Container,
+  VStack,
+  Grid,
+  Box,
+  Heading,
+  Button,
+  Center,
+  Avatar,
+  Text,
+  GridItem,
+} from "@chakra-ui/react";
+import { useAuthContext } from "../../hooks/useAuthContext";
 
 const Welcome = () => {
-  const { user } = useAuthContext()
+  const { user } = useAuthContext();
   return (
-    <Container w='100%' mb={5} pb={5} pl={5} pr={5}>
-        <Heading as='h3'  mt={10} mb={5} >Hello, {user && user.displayName}. Need any help today?</Heading>
-        <Text w='80%' fontSize="xl">Get the help you need from your existing connections, or reach out to other volunteers!</Text>
+    <Container w="100%" mb={5} pb={5} pl={5} pr={5}>
+      <Heading as="h3" mt={10} mb={5}>
+        Hello, {user && user.displayName}. Need any help today?
+      </Heading>
+      <Text w="80%" fontSize="xl">
+        Get the help you need from your existing connections, or reach out to
+        other volunteers!
+      </Text>
     </Container>
   );
 };
