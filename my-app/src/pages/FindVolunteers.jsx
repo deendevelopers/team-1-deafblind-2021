638fb7e71305e5bf791bac191909499922d6b64d
unknown <h.mohiadin.13@gmail.com>
2021-12-05 19:08:35 +0000

    add illustration and more volunteers for find my volunteers page

--- my-app/src/pages/FindVolunteers.jsx
+++ my-app/src/pages/FindVolunteers.jsx
@@ -6,25 +6,56 @@ import TaskSuggestions from "../components/FindVolunteers/TaskSuggestions";
 const FindVolunteers = () => {
   const [volunteers, setVolunteers] = useState({
     volunteers: [
-      {
-        name: "Kola Tioluwani",
-        location: "London, United Kingdom",
-        skills: ["Sign language proficiency ", "Car owner"],
-        interests: ["Sketching", "Cycling", "Harry Potter", "Italian food"],
-      },
-      {
-        name: "Sarah Ashfield",
-        location: "London, United Kingdom",
-        skills: ["Car owner"],
-        interests: ["Reading", "Gardening", "Cooking"],
-      },
+        {
+          name: "Kola Tioluwani",
+          location: "London, United Kingdom",
+          skills: ["Sign language proficiency, ", "Car owner"],
+          interests: ["Sketching", "Cycling", "Harry Potter", "Italian food"],
+        },
+        {
+          name: "Sarah Ashfield",
+          location: "London, United Kingdom",
+          skills: ["Car owner"],
+          interests: ["Reading", "Gardening", "Cooking"],
+        },
+        {
+          name: "Marvin Davis",
+          location: "London, United Kingdom",
+          skills: ["Sign language proficiency, ", "Car owner"],
+          interests: ['Sketching', 'Painting', 'Camping', 'Mountain climbing'],
+        },
+        {
+          name: "Mary Alexander",
+          location: "London, United Kingdom",
+          skills: [ "Car owner"],
+          interests: ['Books', 'Tea', 'Plants', 'Pilates'],
+        },
+        {
+          name: "Sameer Jain",
+          location: "London, United Kingdom",
+          skills: ["Sign language proficiency, ", "Car owner"],
+          interests: ['Technology', 'Football', 'Literature', 'Coffee'],
+        },
+        {
+          name: "Peter O'Connor",
+          location: "London, United Kingdom",
+          skills: ["Car owner"],
+          interests: ['Shopping', 'Fashion', 'Literature', 'Plants'],
+        },
+        {
+          name: "Jonathan Deltevosky",
+          location: "London, United Kingdom",
+          skills: ["Sign language proficiency, ", "Car owner"],
+          interests: ['Mountain climbing', 'Aerobics', 'Literature'],
+        },
+      
     ],
   });
   const allVolunteers = [
     {
       name: "Kola Tioluwani",
       location: "London, United Kingdom",
-      skills: ["Sign language proficiency ", "Car owner"],
+      skills: ["Sign language proficiency, ", "Car owner"],
       interests: ["Sketching", "Cycling", "Harry Potter", "Italian food"],
     },
     {
@@ -33,10 +64,40 @@ const FindVolunteers = () => {
       skills: ["Car owner"],
       interests: ["Reading", "Gardening", "Cooking"],
     },
-  ];
+    {
+      name: "Marvin Davis",
+      location: "London, United Kingdom",
+      skills: ["Sign language proficiency, ", "Car owner"],
+      interests: ['Sketching', 'Painting', 'Camping', 'Mountain climbing'],
+    },
+    {
+      name: "Mary Alexander",
+      location: "London, United Kingdom",
+      skills: [ "Car owner"],
+      interests: ['Books', 'Tea', 'Plants', 'Pilates'],
+    },
+    {
+      name: "Sameer Jain",
+      location: "London, United Kingdom",
+      skills: ["Sign language proficiency, ", "Car owner"],
+      interests: ['Technology', 'Football', 'Literature', 'Coffee'],
+    },
+    {
+      name: "Peter O'Connor",
+      location: "London, United Kingdom",
+      skills: ["Car owner"],
+      interests: ['Shopping', 'Fashion', 'Literature', 'Plants'],
+    },
+    {
+      name: "Jonathan Deltevosky",
+      location: "London, United Kingdom",
+      skills: ["Sign language proficiency, ", "Car owner"],
+      interests: ['Mountain climbing', 'Aerobics', 'Literature'],
+    },
+  ];;
   const filterVolunteers = (task) => {
     const filteredVolunteers = allVolunteers.filter((volunteer) => {
-      return volunteer.skills.includes("Sign language proficiency ");
+      return volunteer.skills.includes("Sign language proficiency, ");
     });
     setVolunteers({ volunteers: filteredVolunteers });
   };
@@ -46,7 +107,7 @@ const FindVolunteers = () => {
         <TaskSuggestions filterVolunteers={filterVolunteers} />
       </Box>
       <Box>
-        <Suggestions volunteers={volunteers.volunteers} heading={'Here are some skilled volunteers ready to help you with your task!'}/>
+        <Suggestions volunteers={volunteers.volunteers} heading={'Here are some skilled volunteers ready to help you with this task!'}/>
       </Box>
     </Grid>
   );
