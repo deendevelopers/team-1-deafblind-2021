039dd006cfb3e9c8326ab7e10191965fd3d993fc
unknown <h.mohiadin.13@gmail.com>
2021-12-06 11:57:14 +0000

    add different users to display connections

--- my-app/src/components/Dashboard/Connection.jsx
+++ my-app/src/components/Dashboard/Connection.jsx
@@ -1,61 +1,27 @@
 import React from "react";
-import { Container, VStack, Grid, Box, Heading, Center, Button, Avatar, Text, GridItem } from '@chakra-ui/react'
+import { Container, VStack, Grid, Box, Heading, Center, Button, Avatar, Text, GridItem, Stack } from '@chakra-ui/react'
 
 const Connection = () => {
+  const connections = [{name: 'Yawar Choudhury', initials: 'YC'}, {name: 'Ahmed Javed', initials: 'AJ'}, {name: 'Eleanor Vandorf', initials: 'EV'}, {name: 'Irene Harf', initials: 'IF'}]
   return (
       <Grid templateColumns='repeat(4, 1fr)'>
-        <Center> 
-          <Box>
-          <Center>
-            <Avatar
-            bg="#D8D8D8"
-                  size='lg'
-                  name='Kola Tioluwani'
-                  src='https://bit.ly/tioluwani-kolawole'
-            /> 
-             </Center>
-            <Text mt={1} fontSize='lg'>Kola Tioluwani</Text>
-          </Box>
-        </Center>
-        <Center> 
-          <Box>
-          <Center>
-            <Avatar
-            bg="#D8D8D8"
-                  size='lg'
-                  name='Kola Tioluwani'
-                  src='https://bit.ly/tioluwani-kolawole'
-            />
-             </Center>
-            <Text fontSize='lg'>Kola Tioluwani</Text>
-          </Box>
-        </Center>
-        <Center> 
-          <Box>
-          <Center>
-            <Avatar
-            bg="#D8D8D8"
-                  size='lg'
-                  name='Kola Tioluwani'
-                  src='https://bit.ly/tioluwani-kolawole'
-            />
-             </Center>
-            <Text fontSize='lg'>Kola Tioluwani</Text>
-          </Box>
-        </Center> 
-        <Center> 
-          <Box>
-          <Center>
-            <Avatar
-            bg="#D8D8D8"
-                  size='lg'
-                  name='Kola Tioluwani'
-                  src='https://bit.ly/tioluwani-kolawole'
-            />
-             </Center>
-            <Text fontSize='lg'>Kola Tioluwani</Text>
-          </Box>
-        </Center> 
+        {connections.map(connection => {
+          return (
+            <Center> 
+            <Box>
+            <Center>
+              <Avatar
+              bg="#D8D8D8"
+                    size='lg'
+                    name={connection.name}
+
+              /> 
+               </Center>
+              <Text mt={1} fontSize='lg'>{connection.name}</Text>
+            </Box>
+          </Center>
+          )
+        })}
       </Grid>
   );
 };
